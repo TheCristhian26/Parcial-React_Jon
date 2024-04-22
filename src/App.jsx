5bab4d8c4128782c27c03ac5e4dbeeebbcccd3e5
rivascristian28@gmail.com <rivascristian28@gmail.com>
2024-04-21 21:48:04 -0500

    Benito Vistas

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { MenuApp } from './components/MenuApp'
+import { MejorCalificadasApp } from './route/MejorCalificadasApp'
+import { PeliculasCArteleraApp } from './route/PeliculasCArteleraApp'
+import { EstrenosApp } from './route/EstrenosApp'
+import { DescripcioApp } from './route/DescripcioApp'
+import { Route, Routes } from 'react-router-dom'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+    <MenuApp/>
+    <Routes>
+      <Route path='/' element={<EstrenosApp></EstrenosApp>}></Route>
+      <Route path='calificadas' element={<MejorCalificadasApp></MejorCalificadasApp>}></Route>
+      <Route path='peliculas' element={<PeliculasCArteleraApp></PeliculasCArteleraApp>}></Route>
+      <Route path='/detalle/:id' element={<DescripcioApp></DescripcioApp>}></Route>
+    </Routes>
     </>
   )
 }
